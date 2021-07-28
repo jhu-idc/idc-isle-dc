c6ae626bd772d598e90787fc02128548958f3cc0
Bethany Seeger <bseeger@jhu.edu>
2021-07-28 11:12:57 -0400

    Changes flag that's used to decide which docker file to change.

--- tests/.includes.sh
+++ tests/.includes.sh
@@ -133,15 +133,17 @@ function use_env {
 	cat .env > "${ENV_FILE}"
 	echo "$1" >> "${ENV_FILE}"
 
-	echo -e "TEST_ENVIRONMENT is '$TEST_ENVIRONMENT'\n"
+	echo -e "CI is '${CI}'\n"
 
-	if [ "$TEST_ENVIRONMENT" == "static" ]; then
-		echo "Using static environment"
-		make -B static-docker-compose.yml env="${ENV_FILE}";
-	else
+	#if [ "$TEST_ENVIRONMENT" == "static" ]; then
+	if [ -z ${CI} ]; then
 		echo "NOT Using static environment"
 		make -B docker-compose.yml args="--env-file ${ENV_FILE}"
+	else
+		echo "Using static environment"
+		make -B static-docker-compose.yml env="${ENV_FILE}";
 	fi
+
 	make up
 	mv .docker-compose.yml docker-compose.yml || make docker-compose.yml
 }
