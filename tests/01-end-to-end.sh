f97621ee4ee431b618f524aad02fc917c6a95812
John Abrahams <jabrah23@gmail.com>
2021-08-04 16:12:21 -0400

    Enable all tests

--- tests/01-end-to-end.sh
+++ tests/01-end-to-end.sh
@@ -1,17 +1,16 @@
 #!/bin/bash
 set -e
 
-TESTCAFE_TESTS_FOLDER="$(pwd)/$(dirname $0)/$(basename $0 .sh)/tests"
+TESTCAFE_TESTS_FOLDER="$(pwd)/end-to-end"
 
 # Start the backend that serves the media files to be migrated
 # Listens internally on port 80 (addressed as http://<assets_container>/assets/)
 startMigrationAssetsContainer
 
-# Run 'admin' end-to-end tests
-docker run --rm --network gateway --env-file "${ENV_FILE}" -v "${TESTCAFE_TESTS_FOLDER}":/end-to-end testcafe/testcafe --screenshots path=/end-to-end/reports,takeOnFails=true chromium /end-to-end/tests/admin/*.spec.js
-
-# Run UI data migrations
-docker run --rm --network gateway --env-file "${ENV_FILE}" -v "${TESTCAFE_TESTS_FOLDER}":/end-to-end testcafe/testcafe --screenshots path=/end-to-end/reports,takeOnFails=true chromium /end-to-end/tests/ui/data-*.js
-
-# Run UI tests
-docker run --rm --network gateway --env-file "${ENV_FILE}" -v "${TESTCAFE_TESTS_FOLDER}":/end-to-end testcafe/testcafe --screenshots path=/end-to-end/reports,takeOnFails=true chromium /end-to-end/tests/ui/*.spec.js
+# 'Admin' tests
+docker run --rm --network gateway --env-file "${ENV_FILE}" -v "${TESTCAFE_TESTS_FOLDER}":/tests testcafe/testcafe --screenshots path=/tests/screenshots,takeOnFails=true chromium /tests/tests/admin/**/*.spec.js
+# Add UI test data
+docker run --rm --network gateway --env-file "${ENV_FILE}" -v "${TESTCAFE_TESTS_FOLDER}":/tests testcafe/testcafe --screenshots path=/tests/screenshots,takeOnFails=true chromium /tests/tests/ui/data-migrations.js
+docker run --rm --network gateway --env-file "${ENV_FILE}" -v "${TESTCAFE_TESTS_FOLDER}":/tests testcafe/testcafe --screenshots path=/tests/screenshots,takeOnFails=true chromium /tests/tests/ui/data-media.js
+# 'UI' tests
+docker run --rm --network gateway --env-file "${ENV_FILE}" -v "${TESTCAFE_TESTS_FOLDER}":/tests testcafe/testcafe --screenshots path=/tests/screenshots,takeOnFails=true chromium /tests/tests/ui/*.spec.js
