6a0c288aef582831ce34c4753ab075d5e88fbf4a
John Abrahams <jabrah23@gmail.com>
2021-08-04 10:36:07 -0400

    Refactor test script

--- tests/01-end-to-end.sh
+++ tests/01-end-to-end.sh
@@ -1,14 +1,17 @@
 #!/bin/bash
 set -e
 
-TESTCAFE_TESTS_FOLDER="$(pwd)/$(dirname $0)/$(basename $0 .sh)/testcafe"
+TESTCAFE_TESTS_FOLDER="$(pwd)/$(dirname $0)/$(basename $0 .sh)/tests"
 
 # Start the backend that serves the media files to be migrated
 # Listens internally on port 80 (addressed as http://<assets_container>/assets/)
 startMigrationAssetsContainer
 
-if [ -t 1 ] ; then
-  docker-compose exec -T testcafe npm test
-else
-  docker-compose exec -ti testcafe npm test
-fi
+# Run 'admin' end-to-end tests
+docker run --rm --network gateway --env-file "${ENV_FILE}" -v "${TESTCAFE_TESTS_FOLDER}":/end-to-end testcafe/testcafe --screenshots path=/end-to-end/reports,takeOnFails=true chromium /end-to-end/tests/admin/*.spec.js
+
+# Run UI data migrations
+docker run --rm --network gateway --env-file "${ENV_FILE}" -v "${TESTCAFE_TESTS_FOLDER}":/end-to-end testcafe/testcafe --screenshots path=/end-to-end/reports,takeOnFails=true chromium /end-to-end/tests/ui/data-*.js
+
+# Run UI tests
+docker run --rm --network gateway --env-file "${ENV_FILE}" -v "${TESTCAFE_TESTS_FOLDER}":/end-to-end testcafe/testcafe --screenshots path=/end-to-end/reports,takeOnFails=true chromium /end-to-end/tests/ui/*.spec.js
