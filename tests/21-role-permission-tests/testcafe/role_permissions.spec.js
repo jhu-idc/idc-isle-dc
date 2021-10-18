463e338ab538079dc866987414f48870dc6c7297
Bethany Seeger <bseeger@jhu.edu>
2021-10-18 14:58:13 -0400

    Fixes tests

--- tests/21-role-permission-tests/testcafe/role_permissions.spec.js
+++ tests/21-role-permission-tests/testcafe/role_permissions.spec.js
@@ -96,27 +96,33 @@ test('Via Migration: Create object user has proper access for', async t => {
 
   await t.navigateTo(`https://islandora-idc.traefik.me/migrate_source_ui`);
 
-  // TODO make this more robust.
-  // Do another migration as the claAdmin.
   // There are two objects in the migration. One should succeed as we put it into
   // a collection the user has permissions to edit. The other one should fail as we
   // try to put it into a collection we do not have permissions to edit.
   await migrate(t, migrate_new_items, './migrations/cla_islandora_objects.csv');
+  await t.wait(5000);
+  // stop here and check the status message.
+  const fileLink = await Selector(".messages--status", { timeout: 10000})
+    .find('li').withText('Processed 2 items (1 created, 0 updated, 1 failed, 0 ignored)');
+  await t.expect(fileLink.count).eql(1);
 
-  await t.navigateTo(`https://islandora-idc.traefik.me/admin/content`);
+  await t.navigateTo('https://islandora-idc.traefik.me/admin/workbench/content/all');
 
   // this should be there
   const found_obj = Selector('div.view-content').find('a').withText('Goat');
   await t.expect(found_obj.count).eql(1);
   await t.click(found_obj);
 
-  await t.navigateTo(`https://islandora-idc.traefik.me/admin/content`);
+  await t.navigateTo('https://islandora-idc.traefik.me/admin/workbench/content/all');
   // this one should fail because they tried to put it into Collection C and they do not
   // have edit access to that.
   const not_found_obj = Selector('div.view-content').find('a').withText('Rhinoceros');
   await t.expect(not_found_obj.count).eql(0);
 
-  // check messages?
-  //await t.navigateTo('https://islandora-idc.traefik.me/admin/structure/migrate/manage/idc_ingest/migrations/idc_ingest_new_items/messages');
+  // lets check the log messages for migrations as well to confirm this.
+  await t.navigateTo('https://islandora-idc.traefik.me/admin/structure/migrate/manage/idc_ingest/migrations/idc_ingest_new_items/messages');
+  const msg = await Selector('.migrate_message_idc_ingest_new_items').find('td').withText('io_cla_02');
+  await t.expect(msg.count).eql(1);
+  await t.expect(msg.parent('tr').child('td').nth(2).innerText).contains('[node]: field_member_of=The user does not have access to ingest into this object.');
 });
 
