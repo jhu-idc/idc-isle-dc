4041725ace66a6ab445d771fb7f3de4b4a62bbf5
John Abrahams <jabrah23@gmail.com>
2021-12-20 14:17:54 -0500

    Add more query syntax tests for global search

--- end-to-end/tests/ui/global-search.spec.js
+++ end-to-end/tests/ui/global-search.spec.js
@@ -9,7 +9,7 @@ test('A basic single word search', async (t) => {
   await t.expect(Results.results.count).eql(4);
 });
 
-test('Boolean search: "yellow AND duck"', async (t) => {
+test('Boolean search: "duck AND yellow"', async (t) => {
   await GlobalSearch.search('yellow AND duck', t);
   await t.expect(Results.results.count).eql(1);
 });
@@ -19,12 +19,28 @@ test('Boolean search: "s3 OR duck"', async (t) => {
   await t.expect(Results.results.count).eql(6);
 });
 
-test('Simple wildcard search with "*"', async (t) => {
+test('Simple wildcard with "*": "test*"', async (t) => {
   await GlobalSearch.search('test*', t);
   await t.expect(Results.results.count).eql(5);
 });
 
+test('Simple windcard with "?": "te?t"', async (t) => {
+  await GlobalSearch.search('te?t', t);
+  await t.expect(Results.results.count).eql(6);
+});
+
 test('Use of double quotes to search exact phrase: "Copyright Undetermined"', async (t) => {
+  // This should omit items marked as "In Copyright" for example
   await GlobalSearch.search('"Copyright Undetermined"', t);
   await t.expect(Results.results.count).eql(4);
+});
+
+test('Compound search: "(duck AND toy) OR goat"', async (t) => {
+  await GlobalSearch.search('(duck AND toy) OR goat', t);
+  await t.expect(Results.results.count).eql(2);
+});
+
+test('Proximity search: \'"test render"~3\'', async (t) => {
+  await GlobalSearch.search('"test render"~3', t);
+  await t.expect(Results.results.count).eql(1);
 });
