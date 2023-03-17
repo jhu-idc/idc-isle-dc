888c521118f690a67cd93c9846fd230b5131e012
Don Richards <highlander411@gmail.com>
2023-03-17 13:49:58 -0400

    Add root dir as variable

--- scripts/host_prep.sh
+++ scripts/host_prep.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 
-cd /var/idc-isle-dc/codebase/ || exit 1
+ROOT_DIR='/var/idc-isle-dc/'
+cd ${ROOT_DIR} || exit 1
 
 if [ -f /etc/server-type.conf ]; then
 	servertypename=$(cat /etc/server-type.conf)
@@ -9,20 +10,21 @@ if [ -f /etc/server-type.conf ]; then
 			echo "File Missing /etc/domain.conf"
 			exit 1
 		fi
+
 		domainname=$(cat /etc/domain.conf)
 		sed -i "s/DOMAIN=.*/DOMAIN=$domainname/g" .env
 
-		if [ ! -f /var/idc-isle-dc/codebase/certs/privkey.pem.bak ]; then
-			echo "Copying /var/idc-isle-dc/codebase/certs/privkey.pem to /var/idc-isle-dc/codebase/certs/privkey.pem.bak"
-			cp /var/idc-isle-dc/codebase/certs/privkey.pem /var/idc-isle-dc/codebase/certs/privkey.pem.bak
+		if [ ! -f ${ROOT_DIR}certs/privkey.pem.bak ]; then
+			echo "Copying ${ROOT_DIR}certs/privkey.pem to ${ROOT_DIR}certs/privkey.pem.bak"
+			cp ${ROOT_DIR}certs/privkey.pem ${ROOT_DIR}certs/privkey.pem.bak
 		fi
-		sudo openssl rsa -in /etc/pki/tls/private/star_mse.jhu.edu.key -text > /var/idc-isle-dc/codebase/certs/privkey.pem || exit 1
+		sudo openssl rsa -in /etc/pki/tls/private/star_mse.jhu.edu.key -text > ${ROOT_DIR}certs/privkey.pem || exit 1
 
-		if [ ! -f /var/idc-isle-dc/codebase/certs/cert.pem.bak ]; then
-			echo "Copying /var/idc-isle-dc/codebase/certs/cert.pem to /var/idc-isle-dc/codebase/certs/cert.pem.bak"
-			cp /var/idc-isle-dc/codebase/certs/cert.pem /var/idc-isle-dc/codebase/certs/cert.pem.bak
+		if [ ! -f ${ROOT_DIR}certs/cert.pem.bak ]; then
+			echo "Copying ${ROOT_DIR}certs/cert.pem to ${ROOT_DIR}certs/cert.pem.bak"
+			cp ${ROOT_DIR}certs/cert.pem ${ROOT_DIR}certs/cert.pem.bak
 		fi
-		sudo cp /etc/pki/tls/certs/star_mse.jhu.edu.pem /var/idc-isle-dc/codebase/certs/cert.pem || exit 1
+		sudo cp /etc/pki/tls/certs/star_mse.jhu.edu.pem ${ROOT_DIR}certs/cert.pem || exit 1
 
 		docker-compose restart traefik
 	fi
