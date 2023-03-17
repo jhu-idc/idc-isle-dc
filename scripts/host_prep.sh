09a2e60f1cb91059e0151370cb88f0d92105d7a3
Don Richards <highlander411@gmail.com>
2023-03-17 13:46:35 -0400

    Fix typo

--- scripts/host_prep.sh
+++ scripts/host_prep.sh
@@ -10,14 +10,16 @@ if [ -f /etc/server-type.conf ]; then
 			exit 1
 		fi
 		domainname=$(cat /etc/domain.conf)
-		# Replace the "DOMAIN=" in the .env file with the one from the server.
 		sed -i "s/DOMAIN=.*/DOMAIN=$domainname/g" .env
 
 		if [ ! -f /var/idc-isle-dc/codebase/certs/privkey.pem.bak ]; then
+			echo "Copying /var/idc-isle-dc/codebase/certs/privkey.pem to /var/idc-isle-dc/codebase/certs/privkey.pem.bak"
 			cp /var/idc-isle-dc/codebase/certs/privkey.pem /var/idc-isle-dc/codebase/certs/privkey.pem.bak
 		fi
-		sudo openssl rsa -in /etc/pki/tls/private/star_mse.jhu.edu.key -text /var/idc-isle-dc/codebase/certs/privkey.pem || exit 1
+		sudo openssl rsa -in /etc/pki/tls/private/star_mse.jhu.edu.key -text > /var/idc-isle-dc/codebase/certs/privkey.pem || exit 1
+
 		if [ ! -f /var/idc-isle-dc/codebase/certs/cert.pem.bak ]; then
+			echo "Copying /var/idc-isle-dc/codebase/certs/cert.pem to /var/idc-isle-dc/codebase/certs/cert.pem.bak"
 			cp /var/idc-isle-dc/codebase/certs/cert.pem /var/idc-isle-dc/codebase/certs/cert.pem.bak
 		fi
 		sudo cp /etc/pki/tls/certs/star_mse.jhu.edu.pem /var/idc-isle-dc/codebase/certs/cert.pem || exit 1
@@ -25,4 +27,3 @@ if [ -f /etc/server-type.conf ]; then
 		docker-compose restart traefik
 	fi
 fi
-
